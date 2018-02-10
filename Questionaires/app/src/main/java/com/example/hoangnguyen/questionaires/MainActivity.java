e3cff3594f8abbd825ea2e57d5666e0c5033c43c
hoangNguyen2008 <hoangnguyen@Hoangs-MacBook-Pro.local>
2018-02-10 16:41:42 -0600

    Add project files

--- Questionaires/app/src/main/java/com/example/hoangnguyen/questionaires/MainActivity.java
+++ Questionaires/app/src/main/java/com/example/hoangnguyen/questionaires/MainActivity.java
@@ -9,7 +9,6 @@ import android.view.TextureView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.graphics.PorterDuff;
 
 import java.util.Random;
 
