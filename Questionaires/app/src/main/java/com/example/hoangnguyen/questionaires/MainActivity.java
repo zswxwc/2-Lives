10f906b644e633a9f1d8c287ca26bffdf4fc21fd
TheRyanKingII <34190419+TheRyanKingII@users.noreply.github.com>
2018-02-10 16:19:24 -0600

    almost

--- Questionaires/app/src/main/java/com/example/hoangnguyen/questionaires/MainActivity.java
+++ Questionaires/app/src/main/java/com/example/hoangnguyen/questionaires/MainActivity.java
@@ -9,6 +9,7 @@ import android.view.TextureView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.graphics.PorterDuff;
 
 import java.util.Random;
 
